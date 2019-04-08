47f049e6f3348deb1060bdfe3cc041b9a2d9b6e7
Irene <43371161+irenetaylor@users.noreply.github.com>
2019-04-08 13:00:51 +0100

    Add files via upload

---
+++ Input.R
@@ -0,0 +1,6 @@
+print("Select character class:")
+print("1 - ManMan")
+print("2 - TurtleBoy")
+print("3 - RatMan")
+playerOne <- readline(prompt="Player 1: ");playerTwo <- readline(prompt="Player 2: ")
+
